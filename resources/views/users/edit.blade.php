ede7a5b766349cdb5ba7e2dbd16e3c3df981dcf3
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-02-27 15:16:12 +0100

    added option to edit password
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -65,27 +65,57 @@
                 </div>
             </div>
 
-            <div class="form-group row">
-                <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Rol') }}</label>
+            @if(\Auth::user()->isAn('admin'))
+                @includeIf('users.partials.input-role')
+            @endif
 
-                <div class="col-md-6">
-                    <select id="role" type="text" class="custom-select{{ $errors->has('role') ? ' is-invalid' : '' }}" name="role" value="{{ old('role') }}" required>
-                        @foreach(DB::table('roles')->select('*')->where('name', '!=', 'banned')->get() as $role)
-                            <option value="{{ $role->name }}" {{ in_array($role->name, $user->getRoles()->toArray(), true)? 'selected': '' }}>{{ $role->title }}</option>
-                        @endforeach
-                    </select>
-
-                    @if ($errors->has('role'))
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $errors->first('role') }}</strong>
-                        </span>
-                    @endif
-                </div>
-            </div>
-
-            <button type="submit" class="btn btn-primary">Aanpassen</button>
+            @include('layouts.partials.forms.button', ['type' => 'submit', 'text' => 'Aanpassen'])
         </form>
     </div>
 </div>
+
+@if((\Auth::user()->id === $user->id) || \Auth::user()->isAn('admin'))
+    <div class="card mt-4">
+        <div class="card-header">
+            Wachtwoord aanpassen
+        </div>
+        <div class="card-body">
+            <form action="{{ route('user.edit.password', $user) }}" method="POST">
+                @method('patch')
+                @csrf
+                <div class="form-group row">
+                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Nieuw wachtwoord') }}</label>
+
+                    <div class="col-md-6">
+                        <input id="password" type="text" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" value="{{ old('password') }}" required autofocus>
+
+                        @if ($errors->has('password'))
+                            <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="password_confirmation" class="col-md-4 col-form-label text-md-right">{{ __('Bevestig wachtwoord') }}</label>
+
+                    <div class="col-md-6">
+                        <input id="password_confirmation" type="text" class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}" name="password_confirmation" required>
+
+                        @if ($errors->has('password_confirmation'))
+                            <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('password_confirmation') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                </div>
+                <div class="form-group">
+                    @include('layouts.partials.forms.button', ['type' => 'submit', 'text' => 'Aanpassen'])
+                </div>
+            </form>
+        </div>
+    </div>
+@endif
 
 @endsection
