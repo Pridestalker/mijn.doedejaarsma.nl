f6926bbee870051c74a885b7536ae5a9fa05c638
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-03-08 09:17:42 +0100

    Style feedback
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,6 @@ Vue.config.productionTip = false;
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
-
 if (token) {
     axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
---
+++ resources/js/components/BlockLoaderComponent.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="loading" :class="size"></div>
+</template>
+
+<script>
+    export default {
+        name: "BlockLoaderComponent",
+        props: {
+            size: {
+                type: String,
+                default: 'normal'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+@keyframes placeholderShimmer {
+    0% {
+        background-position: -468px 0
+    }
+    100% {
+        background-position: 468px 0
+    }
+}
+
+.loading {
+    animation-duration: 1s;
+    animation-fill-mode: forwards;
+    animation-iteration-count: infinite;
+    animation-name: placeholderShimmer;
+    animation-timing-function: linear;
+    background: #f6f7f8;
+    background: linear-gradient(to right, #eeeeee 8%, #ef8716 18%, #eeeeee 33%);
+    background-size: 800px 104px;
+    position: relative;
+}
+
+.loading.normal {
+    height: 96px;
+}
+
+.loading.small {
+    height: 40px;
+}
+
+.loading.xsmall {
+    height: 1rem;
+}
+
+.loading.large {
+    height: 15rem;
+}
+</style>
