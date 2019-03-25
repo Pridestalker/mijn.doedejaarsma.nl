2635fd3be3508d08e5ae6ed3e1f3349e400bdc27
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-03-25 15:23:53 +0100

    Added a service to safe products from one spot
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

---
+++ app/Http/Controllers/Services/ProductService.php
@@ -0,0 +1,103 @@
+<?php
+namespace App\Http\Controllers\Services;
+
+use App\Events\Product\ProductCreatedEvent;
+use App\Models\Product;
+use App\User;
+use Auth;
+use Error;
+use Exception;
+use Illuminate\Http\Request;
+use Log;
+
+class ProductService
+{
+    protected $model = Product::class;
+    
+    protected $request;
+    
+    protected $product;
+    
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+    
+    public function store()
+    {
+        $validated = $this->request->validate(
+            [
+                'name'      => 'required|string',
+                'deadline'  => 'required',
+                'soort'     => 'required',
+            ]
+        );
+        
+        if ($this->hasFile()) {
+            $path = $this->storeFile();
+        }
+    
+        try {
+            $atts = $this->request->except('attachment');
+            $atts['status'] = 'aangevraagd';
+            $atts['attachment'] = $path?? null;
+            $atts['user_id'] = $this->request->has('user_id') ?
+                $this->request->get('user_id') :
+                Auth::user()->id;
+    
+            $this->product = Product::create($atts);
+            
+            if ($this->hasPrintOptions()) {
+                $data = [
+                    'oplage' => $this->request->get('oplage')?? '',
+                    'papier' => $this->request->get('papier')?? '',
+                    'gewicht'=> $this->request->get('gewicht')?? '',
+                    'afleveradres' => $this->request->get('afleveradres')?? '',
+                ];
+    
+                $this->product->options = json_encode($data);
+            }
+            $this->product->save();
+        } catch (Exception $e) {
+            Log::error($e->getMessage(), $e->getTrace());
+            throw new Error('Error creating product');
+        }
+        
+        
+        if ($this->product->exists()) {
+            if ($this->request->has('user_id')) {
+                event(
+                    new ProductCreatedEvent(
+                        $this->product,
+                        User::findOrFail($this->request->get('user_id'))
+                    )
+                );
+            } else {
+                event(new ProductCreatedEvent($this->product, Auth::user()));
+            }
+            return $this->product;
+        } else {
+            Log::error('Aanvraag error', [ 'user' => Auth::user()]);
+            throw new Error('Error creating product');
+        }
+    }
+    
+    protected function hasPrintOptions(): bool
+    {
+        return $this->request->hasAny(['oplage','papier','gewicht','afleveradres']);
+    }
+    
+    protected function hasFile(): bool
+    {
+        return $this->request->hasFile('attachment');
+    }
+    
+    protected function storeFile()
+    {
+        /* @noinspection NullPointerExceptionInspection */
+        return $this
+            ->request
+            ->file('attachment')
+            ->storePublicly('voorbeelden');
+    }
+}
