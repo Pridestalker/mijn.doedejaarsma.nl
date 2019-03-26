212735ac49c6e6d5155fdf427f5f11ca6125b259
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-03-26 11:21:59 +0100

    notifications remove button fix
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- routes/api.php
+++ routes/api.php
@@ -51,7 +51,7 @@ Route::prefix('v1')->middleware('auth:api')->group(
                         Route::get('/unread', 'Api\V1\User\NotificationsController@unread')
                             ->name('api.user.notifications.unread');
                         
-                        Route::get('/delete/{$id}', 'Api\V1\User\NotificationsController@delete')
+                        Route::get('/delete/{id}', 'Api\V1\User\NotificationsController@delete')
                             ->name('api.user.notifications.delete');
                     }
                 );
