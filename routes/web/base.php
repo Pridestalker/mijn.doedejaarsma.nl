4a645246e9dfbb206422c56dee224f2c678f1af2
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-01-16 08:03:59 +0100

    WIP
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- routes/web/base.php
+++ routes/web/base.php
@@ -10,5 +10,8 @@ Route::get('/downloads/products', 'Resources\ProductController@download')
      ->middleware('auth')
      ->name('download.product.all');
 
+Route::get('/designers/app', function () {
+	return view('designer.app');
+});
 
 Route::impersonate();
