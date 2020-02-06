d509b18710a48573d82ffc6e001ecd68732e6c99
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-02-06 13:19:31 +0100

    Added is_active field to users table; Added boolean cast to is_active property
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

---
+++ database/migrations/2020_02_06_131158_add_active_field_to_users.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddActiveFieldToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('users', static function (Blueprint $table) {
+            $table->boolean('is_active')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('users', static function (Blueprint $table) {
+            $table->dropColumn('is_active');
+        });
+    }
+}
